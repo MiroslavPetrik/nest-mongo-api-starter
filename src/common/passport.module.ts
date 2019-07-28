7c3368c9fd208268a8c21c8e8ad703da66945ff7
Miroslav Petrik <miroslavpetrik@outlook.com>
2019-07-28 14:40:41 +0200

    cleanup e2e tests

--- src/common/passport.module.ts
+++ src/common/passport.module.ts
@@ -4,4 +4,3 @@ import { PassportModule } from '@nestjs/passport';
  * Add to module imports everywhere where AuthGuard with default strategy is to be used.
  */
 export default PassportModule.register({ defaultStrategy: 'jwt' });
-
