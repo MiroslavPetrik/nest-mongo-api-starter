37ec2dd962dd3440b0fc8b1ec7bf1989cc9aff34
Miroslav Petrik <miroslavpetrik@outlook.com>
2022-01-23 19:31:54 +0100

    prettier test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,16 +1,16 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
+import {Test, TestingModule} from "@nestjs/testing";
+import * as request from "supertest";
+import * as dotenv from "dotenv";
+import * as path from "path";
 
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({path: path.resolve(__dirname, ".env")});
 
-import { AppModule } from './../src/app.module';
-import { configureApp } from '../src/bootstrap';
+import {AppModule} from "./../src/app.module";
+import {configureApp} from "../src/bootstrap";
 
-import seeder from './seeder';
+import seeder from "./seeder";
 
-describe('AppController (e2e)', () => {
+describe("AppController (e2e)", () => {
   let app: any;
 
   beforeAll(async () => {
@@ -25,8 +25,8 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  describe('/api/activate/:userId/:activationToken', () => {
-    it('should fail with status 403 when sent invalid activation token', () => {
+  describe("/api/activate/:userId/:activationToken", () => {
+    it("should fail with status 403 when sent invalid activation token", () => {
       return request(app.getHttpServer())
         .get(
           `/api/activate/507f1f77bcf86cd799439011/16d12e06-6119-4b6e-b9c2-6530fd2f591b`,
@@ -34,7 +34,7 @@ describe('AppController (e2e)', () => {
         .expect(403);
     });
 
-    it('should succeed with status 200 when sent valid activation token', () => {
+    it("should succeed with status 200 when sent valid activation token", () => {
       return request(app.getHttpServer())
         .get(
           `/api/activate/507f1f77bcf86cd799439011/16d12e06-6119-4b6e-b9c2-6530fd2f591a`,
@@ -43,60 +43,60 @@ describe('AppController (e2e)', () => {
     });
   });
 
-  describe('/api/login (POST)', () => {
-    it('should fail with status 401 when posted incorrect body params', () => {
+  describe("/api/login (POST)", () => {
+    it("should fail with status 401 when posted incorrect body params", () => {
       return request(app.getHttpServer())
-        .post('/api/login')
-        .send({ incorrect: 'field' })
+        .post("/api/login")
+        .send({incorrect: "field"})
         .expect(401);
     });
 
-    it('should fail with status 404 when email does not exist', async () => {
-      const { status } = await request(app.getHttpServer())
-        .post('/api/login')
-        .send({ email: 'doesnt@exist.com', password: 'password' });
+    it("should fail with status 404 when email does not exist", async () => {
+      const {status} = await request(app.getHttpServer())
+        .post("/api/login")
+        .send({email: "doesnt@exist.com", password: "password"});
 
       expect(status).toBe(404);
     });
 
-    it('should fail with status 401 when posted incorrect credentials', async () => {
-      const { status } = await request(app.getHttpServer())
-        .post('/api/login')
-        .send({ email: 'foo@bar.com', password: 'notInDatabase' });
+    it("should fail with status 401 when posted incorrect credentials", async () => {
+      const {status} = await request(app.getHttpServer())
+        .post("/api/login")
+        .send({email: "foo@bar.com", password: "notInDatabase"});
 
       expect(status).toBe(401);
     });
 
-    it('should succeed with status 201 and return token when posted correct credentials', async () => {
-      const { status, body } = await request(app.getHttpServer())
-        .post('/api/login')
-        .send({ email: 'foo@bar.com', password: 'password' });
+    it("should succeed with status 201 and return token when posted correct credentials", async () => {
+      const {status, body} = await request(app.getHttpServer())
+        .post("/api/login")
+        .send({email: "foo@bar.com", password: "password"});
 
       expect(status).toBe(201);
       expect(body.token).toBeDefined();
     });
   });
 
-  describe('/api/signup (POST)', () => {
-    it('should fail with status 400 when email has wrong form', async () => {
-      const { status } = await request(app.getHttpServer())
-        .post('/api/signup')
-        .send({ email: 'email', password: 'password' });
+  describe("/api/signup (POST)", () => {
+    it("should fail with status 400 when email has wrong form", async () => {
+      const {status} = await request(app.getHttpServer())
+        .post("/api/signup")
+        .send({email: "email", password: "password"});
 
       expect(status).toBe(400);
     });
 
-    it('should fail with status 409 for existing email', () => {
+    it("should fail with status 409 for existing email", () => {
       return request(app.getHttpServer())
-        .post('/api/signup')
-        .send({ email: 'foo@bar.com', password: 'password' })
+        .post("/api/signup")
+        .send({email: "foo@bar.com", password: "password"})
         .expect(409);
     });
 
-    it('should succeed with status 201 and return user with token when valid data provided', async () => {
-      const { status, body } = await request(app.getHttpServer())
-        .post('/api/signup')
-        .send({ email: 'email@email.com', password: 'password' });
+    it("should succeed with status 201 and return user with token when valid data provided", async () => {
+      const {status, body} = await request(app.getHttpServer())
+        .post("/api/signup")
+        .send({email: "email@email.com", password: "password"});
 
       expect(status).toBe(201);
       expect(body.token).toBeDefined();
@@ -104,79 +104,75 @@ describe('AppController (e2e)', () => {
     });
   });
 
-  describe('/api/me (GET)', () => {
+  describe("/api/me (GET)", () => {
     let token: string;
 
     beforeAll(async () => {
-      const { body } = await request(app.getHttpServer())
-        .post('/api/login')
-        .send({ email: 'foo@bar.com', password: 'password' });
+      const {body} = await request(app.getHttpServer())
+        .post("/api/login")
+        .send({email: "foo@bar.com", password: "password"});
 
       token = body.token;
     });
 
-    it('should fail with status 401 when provided invalid authorization header', () => {
-      return request(app.getHttpServer())
-        .get('/api/me')
-        .expect(401);
+    it("should fail with status 401 when provided invalid authorization header", () => {
+      return request(app.getHttpServer()).get("/api/me").expect(401);
     });
 
-    it('should succeed with status 200 when provided valid authorization header', async () => {
-      const { status, body } = await request(app.getHttpServer())
-        .get('/api/me')
-        .set('Authorization', `Bearer ${token}`);
+    it("should succeed with status 200 when provided valid authorization header", async () => {
+      const {status, body} = await request(app.getHttpServer())
+        .get("/api/me")
+        .set("Authorization", `Bearer ${token}`);
 
       expect(status).toBe(200);
       expect(body.email).toBeDefined();
     });
   });
 
-  describe('/api/relogin (GET)', () => {
+  describe("/api/relogin (GET)", () => {
     let token: string;
 
     beforeAll(async () => {
-      const { body } = await request(app.getHttpServer())
-        .post('/api/login')
-        .send({ email: 'foo@bar.com', password: 'password' });
+      const {body} = await request(app.getHttpServer())
+        .post("/api/login")
+        .send({email: "foo@bar.com", password: "password"});
 
       token = body.token;
     });
 
-    it('should succeed with status 200 and return new token when provided valid authorization header', async () => {
-      const { status, body } = await request(app.getHttpServer())
-        .get('/api/relogin')
-        .set('Authorization', `Bearer ${token}`);
+    it("should succeed with status 200 and return new token when provided valid authorization header", async () => {
+      const {status, body} = await request(app.getHttpServer())
+        .get("/api/relogin")
+        .set("Authorization", `Bearer ${token}`);
 
       expect(status).toBe(200);
       expect(body.token).toBeDefined();
     });
 
-    it('should fail with status 401 when provided invalid authorization header', () => {
-      return request(app.getHttpServer())
-        .get('/api/relogin')
-        .expect(401);
+    it("should fail with status 401 when provided invalid authorization header", () => {
+      return request(app.getHttpServer()).get("/api/relogin").expect(401);
     });
   });
 
-  describe('/api/reset-password (POST)', () => {
-    it('should fail with status 403 when provided invalid passwordResetToken', async () => {
-      const { status } = await request(app.getHttpServer())
-        .post('/api/reset-password')
+  describe("/api/reset-password (POST)", () => {
+    it("should fail with status 403 when provided invalid passwordResetToken", async () => {
+      const {status} = await request(app.getHttpServer())
+        .post("/api/reset-password")
         .send({
-          email: 'foo@bar.com',
-          passwordResetToken: '3bdafa11-c3d0-4f15-8811-ce4ab2da2ebb',
-          password: 'newPassword',
+          email: "foo@bar.com",
+          passwordResetToken: "3bdafa11-c3d0-4f15-8811-ce4ab2da2ebb",
+          password: "newPassword",
         });
       expect(status).toBe(403);
     });
 
-    it('should succeed with status 200 and login body when provided valid passwordResetToken', async () => {
-      const { status, body } = await request(app.getHttpServer())
-        .post('/api/reset-password')
+    it("should succeed with status 200 and login body when provided valid passwordResetToken", async () => {
+      const {status, body} = await request(app.getHttpServer())
+        .post("/api/reset-password")
         .send({
-          email: 'foo@bar.com',
-          passwordResetToken: '3bdafa11-c3d0-4f15-8811-ce4ab2da2eba',
-          password: 'newPassword',
+          email: "foo@bar.com",
+          passwordResetToken: "3bdafa11-c3d0-4f15-8811-ce4ab2da2eba",
+          password: "newPassword",
         });
 
       expect(status).toBe(201);
@@ -184,37 +180,37 @@ describe('AppController (e2e)', () => {
       expect(body.user).toBeDefined();
     });
 
-    describe('after password reset', () => {
-      it('should be able to log in with the new password', () => {
+    describe("after password reset", () => {
+      it("should be able to log in with the new password", () => {
         return request(app.getHttpServer())
-          .post('/api/login')
-          .send({ email: 'foo@bar.com', password: 'newPassword' })
+          .post("/api/login")
+          .send({email: "foo@bar.com", password: "newPassword"})
           .expect(201);
       });
     });
   });
 
-  describe('/api/forgotten-password (POST)', () => {
-    it('should fail with status 400 when email malformed', async () => {
-      const { status } = await request(app.getHttpServer())
-        .post('/api/forgotten-password')
-        .send({ email: 'unknown' });
+  describe("/api/forgotten-password (POST)", () => {
+    it("should fail with status 400 when email malformed", async () => {
+      const {status} = await request(app.getHttpServer())
+        .post("/api/forgotten-password")
+        .send({email: "unknown"});
 
       expect(status).toBe(400);
     });
 
-    it('should fail with status 404 when email not found', async () => {
-      const { status } = await request(app.getHttpServer())
-        .post('/api/forgotten-password')
-        .send({ email: 'unknown@email.com' });
+    it("should fail with status 404 when email not found", async () => {
+      const {status} = await request(app.getHttpServer())
+        .post("/api/forgotten-password")
+        .send({email: "unknown@email.com"});
 
       expect(status).toBe(404);
     });
 
-    it('should succeed with status 200 when email found', async () => {
-      const { status } = await request(app.getHttpServer())
-        .post('/api/forgotten-password')
-        .send({ email: 'foo@bar.com' });
+    it("should succeed with status 200 when email found", async () => {
+      const {status} = await request(app.getHttpServer())
+        .post("/api/forgotten-password")
+        .send({email: "foo@bar.com"});
 
       expect(status).toBe(201);
     });
--- test/seeder/index.ts
+++ test/seeder/index.ts
@@ -1,22 +1,22 @@
-import * as mongoose from 'mongoose';
-import { Seeder } from 'mongoose-seed-2';
+import * as mongoose from "mongoose";
+import {Seeder} from "mongoose-seed-2";
 
-import { hashPassword } from '../../src/common/auth';
-import { UserSchema } from '../../src/user/user.schema';
-import config from '../../src/config';
+import {hashPassword} from "../../src/common/auth";
+import {UserSchema} from "../../src/user/user.schema";
+import config from "../../src/config";
 
-const UserModel = mongoose.model('User', UserSchema);
+const UserModel = mongoose.model("User", UserSchema);
 
 const seeder = new Seeder(process.env.MONGO_URL!);
 
 export default {
   async initialize() {
     const data = await getData();
-    await seeder.clearModels(['User']);
+    await seeder.clearModels(["User"]);
     await UserModel.collection.drop();
     await seeder.populateModels(data);
 
-    (console as any).info('DB seed was successful');
+    (console as any).info("DB seed was successful");
   },
 
   async disconnect() {
@@ -28,14 +28,14 @@ async function getData() {
   return {
     User: [
       {
-        _id: '507f1f77bcf86cd799439011',
-        email: 'foo@bar.com',
+        _id: "507f1f77bcf86cd799439011",
+        email: "foo@bar.com",
         isActive: false,
-        activationToken: '16d12e06-6119-4b6e-b9c2-6530fd2f591a',
+        activationToken: "16d12e06-6119-4b6e-b9c2-6530fd2f591a",
         activationExpires: Date.now() + config.auth.activationExpireInMs,
-        passwordResetToken: '3bdafa11-c3d0-4f15-8811-ce4ab2da2eba',
+        passwordResetToken: "3bdafa11-c3d0-4f15-8811-ce4ab2da2eba",
         passwordResetExpires: Date.now() + config.auth.passwordResetExpireInMs,
-        password: await hashPassword('password'),
+        password: await hashPassword("password"),
       },
     ],
   };
