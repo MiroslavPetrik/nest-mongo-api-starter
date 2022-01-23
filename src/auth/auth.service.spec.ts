e4b95f4d8ad543a2301b3060eb534e1cbc996c5b
Miroslav Petrik <miroslavpetrik@outlook.com>
2022-01-23 21:13:30 +0100

    mock auth service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,18 @@
 import {Test, TestingModule} from "@nestjs/testing";
+import {JwtService} from "@nestjs/jwt";
+
 import {AuthService} from "./auth.service";
+import {UserService} from "../user/user.service";
+
+jest.mock("@nestjs/jwt");
+jest.mock("../user/user.service.ts");
 
 describe("AuthService", () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [UserService, JwtService, AuthService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
